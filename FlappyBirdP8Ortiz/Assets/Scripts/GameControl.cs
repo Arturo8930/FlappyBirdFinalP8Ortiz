017f709e6bbe582ba39b9c337886e7255ac2f5f1
Arturo Ortiz <753788@student.dusd.net>
2024-12-11 15:30:07 -0800

    Flappy Bird Final
    Finally finished Project for unity

--- FlappyBirdP8Ortiz/Assets/Scripts/GameControl.cs
+++ FlappyBirdP8Ortiz/Assets/Scripts/GameControl.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     // Start is called before the first frame update
     void Awake ()
@@ -31,6 +36,16 @@ public class GameControl : MonoBehaviour
         {
             SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
         }
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
     }
 
     public void BirdDied()
